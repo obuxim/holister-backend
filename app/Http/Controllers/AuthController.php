9cbe5c4bd7a06b3eadbd8be1bdeb0e42ae01d55d
Zubair Hasan <z@duoneos.com>
2021-04-10 15:21:10 +0600

    Registration Endpoint Done

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    // Register and send back token
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string|unique:users,email',
+            'name' => 'required|string',
+            'phone' => 'required|string',
+            'gender' => 'string|required',
+            'profile_picture' => 'nullable|file',
+            'password' => 'string|required|confirmed'
+        ]);
+        $user = User::create([
+            'email' => $fields['email'],
+            'name' => $fields['name'],
+            'phone' => $fields['phone'],
+            'gender' => $fields['gender'],
+            'password' => Hash::make($fields['password']),
+        ]);
+        if($request->hasFile('profile_picture')){
+            $originalFileName = $request->file('profile_picture')->getClientOriginalName();
+            $fileExt = $request->file('profile_picture')->getClientOriginalExtension();
+            $originalFileNameWithoutExt = Str::of($originalFileName)->basename('.'.$fileExt);
+            $fileNameToSave = $originalFileNameWithoutExt . '_' . time() . '.' . $fileExt;
+            $user->profile_picture = 'profile_picture/'.$fileNameToSave;
+            $user->save();
+            $request->file('profile_picture')->storeAs('public/profile_picture', $fileNameToSave);
+        }
+
+        $response = new \stdClass();
+        $response->user = $user;
+        $response->token = $user->createToken(config('app.key'))->plainTextToken;
+
+        return response()->json($response);
+    }
+}
